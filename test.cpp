7c376ffb967eab78ddf9924ab4ae138ec2ff6364
陈安卓 <chenanzhuo517@gmail.com>
2020-05-20 15:37:38 +0800

    Update test.cpp
    add output test  content

--- test.cpp
+++ test.cpp
@@ -11,7 +11,13 @@ int main()
 	std::tuple<std::wstring, std::wstring> params_(TEXT("userid"), TEXT("name"));
 	std::vector<std::tuple<int, int>> content_;
 	sql_.select(std::wstring(TEXT("select * from game.dbo.gameuserinfo;")), content_, params_);
-
+	
+	for (auto i = 0; i < content_.size(); ++i)
+	{
+		auto val = content_[i];
+		std::cout << std::get<0>(val) << " + " << std::get<1>(val) << "\n";
+	}
+	
 	return 0;
 }
 
