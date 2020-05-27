622764525645d3fcb952ee726e3f8ef94008cbb3
陈安卓 <chenanzhuo517@gmail.com>
2020-05-27 16:14:28 +0800

    范例string类型调整为wstring

--- test.cpp
+++ test.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "sql_server_warpper.h"
 
@@ -11,21 +10,22 @@ int main()
 	// 创建
 	sql_.invoke(std::wstring(TEXT("create table game.dbo.test (name VARCHAR(50) NOT NULL, id INT NOT NULL);")));
 	// 增加
-	sql_.insert(std::wstring(TEXT("insert into game.dbo.test (name, id) values('hello', 123); ")));
+	sql_.insert(std::wstring(TEXT("insert into game.dbo.test (name, id) values('hello', 123);")));
 	// 删除
-	sql_.remove(std::wstring(TEXT("delete top 10 from game.dbo.test;")));
+	sql_.remove(std::wstring(TEXT("delete top(1) from game.dbo.test;")));
 	// 修改
 	sql_.update(std::wstring(TEXT("update game.dbo.test set id = 2000000 where name = 'hello';")));
 
 	// 查询
 	std::tuple<std::wstring, std::wstring> params_(TEXT("name"), TEXT("id"));
-	std::vector<std::tuple<std::string, int>> content_;
+	std::vector<std::tuple<std::wstring, int>> content_;
 	sql_.select(std::wstring(TEXT("select * from game.dbo.test;")), content_, params_);
 	for (std::size_t i = 0; i < content_.size(); ++i)
 	{
 		auto val = content_[i];
-		std::cout << std::get<0>(val) << " + " << std::get<1>(val) << "\n";
+		std::wcout << std::get<0>(val) << " + " << std::get<1>(val) << "\n";
 	}
 
 	return 0;
 }
+
