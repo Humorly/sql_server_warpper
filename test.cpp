01c069c515eabfdb203265fde05a5faa6da530ac
陈安卓 <chenanzhuo517@gmail.com>
2020-05-27 11:26:38 +0800

    更新测试接口

--- test.cpp
+++ test.cpp
@@ -1,30 +1,31 @@
 
 #include <iostream>
 #include "sql_server_warpper.h"
-#include <tchar.h>
 
 int main()
 {
-	sql_server_warpper sql_(std::wstring(TEXT("PROVIDER=SQLOLEDB;DATABASE=master;Data Source=127.0.0.1,1456;")),
-		std::wstring(TEXT("admin")), std::wstring(TEXT("aaa123")));
+	// 配置用户名、密码、地址、端口
+	sql_server_warpper sql_(std::wstring(TEXT("PROVIDER=SQLOLEDB;DATABASE=master;Data Source=127.0.0.1,9977;")),
+		std::wstring(TEXT("root")), std::wstring(TEXT("778779")));
 
-	std::tuple<std::wstring, std::wstring> params_(TEXT("userid"), TEXT("name"));
-	std::vector<std::tuple<int, int>> content_;
-	sql_.select(std::wstring(TEXT("select * from game.dbo.gameuserinfo;")), content_, params_);
-	
-	for (auto i = 0; i < content_.size(); ++i)
+	// 创建
+	sql_.invoke(std::wstring(TEXT("create table game.dbo.test (name VARCHAR(50) NOT NULL, id INT NOT NULL);")));
+	// 增加
+	sql_.insert(std::wstring(TEXT("insert into game.dbo.test (name, id) values('hello', 123); ")));
+	// 删除
+	sql_.remove(std::wstring(TEXT("delete top 10 from game.dbo.test;")));
+	// 修改
+	sql_.update(std::wstring(TEXT("update game.dbo.test set id = 2000000 where name = 'hello';")));
+
+	// 查询
+	std::tuple<std::wstring, std::wstring> params_(TEXT("name"), TEXT("id"));
+	std::vector<std::tuple<std::string, int>> content_;
+	sql_.select(std::wstring(TEXT("select * from game.dbo.test;")), content_, params_);
+	for (std::size_t i = 0; i < content_.size(); ++i)
 	{
 		auto val = content_[i];
 		std::cout << std::get<0>(val) << " + " << std::get<1>(val) << "\n";
 	}
-	
+
 	return 0;
 }
-
-
-
-
-
-
-
-
